e66dac144e2de079fd51b8d4e408923c2935aa5d
Mikael Yeghiazaryan <mikaegiazaryan2000@gmail.com>
2022-07-19 16:08:39 +0400

    Added a script to evaluate the results per object (plotting histograms)

--- cosypose/utils/results_stats.py
+++ cosypose/utils/results_stats.py
@@ -1,5 +1,7 @@
 import json
 import os
+from matplotlib import pyplot as plt
+import numpy as np
 
 """Functions"""
 def read_file(file_path):
@@ -23,6 +25,33 @@ def load_results(files_paths):
         files.append(result)
     return files
 
+def generate_object_stats(results):
+    """This function generates statistics for the 30 objects from the T-LESS data set using the results supplied."""
+    stats = {i: list() for i in range(1, 31)}
+    for scene in results:
+        for obj in scene:
+            error_val = max(obj['errors'].values())[0]
+            obj_id = obj['obj_id']
+            stats[obj_id].append(error_val)
+    return stats
+    
+def plot_results(stats, idx=5, num_bins=10):
+    """Variable idx represents either the object id or the scene id."""
+    stats_ = stats[idx]
+    
+    # Build the figure and the histogram
+    plt.hist(stats_, bins=np.linspace(0.0, 1.0, num=num_bins+1))
+    plt.title("Errors histogram")
+    plt.xlabel("Error value")
+    plt.ylabel("Frequency")
+    plt.show()
+
+
 """Main code"""
+# Load the results
 files_paths = get_files()
 results = load_results(files_paths)
+
+# Generate statistics per object
+stats = generate_object_stats(results)
+plot_results(stats, idx=5, num_bins=10)
